fa2b29cac3772b3f307a53115df69e4d8da8d451
Clelia <clelia.clp3z@gmail.com>
2025-04-05 10:14:21 +0200

    feat: Created shared gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,51 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
-    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.jetbrains.kotlin.kapt) apply false
+    alias(libs.plugins.google.dagger.hilt) apply false
+    alias(libs.plugins.jetbrains.kotlin.serialization) apply false
+}
+
+subprojects {
+    afterEvaluate {
+        plugins.withId("com.android.library") {
+            val androidLibrary = extensions.findByType<com.android.build.gradle.LibraryExtension>()
+            androidLibrary?.apply {
+                compileSdk = 35
+
+                defaultConfig {
+                    minSdk = 24
+                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                    consumerProguardFiles("consumer-rules.pro")
+                }
+
+                buildTypes {
+                    release {
+                        isMinifyEnabled = false
+                        proguardFiles(
+                            getDefaultProguardFile("proguard-android-optimize.txt"),
+                            "proguard-rules.pro"
+                        )
+                    }
+                }
+
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+
+            plugins.withId("org.jetbrains.kotlin.android") {
+                extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension> {
+                    compilerOptions {
+                        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+                    }
+                }
+            }
+        }
+    }
 }
--- framework/network/build.gradle.kts
+++ framework/network/build.gradle.kts
@@ -1,8 +1,16 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.dagger.hilt)
+    alias(libs.plugins.jetbrains.kotlin.kapt)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
 }
 
+android {
+    namespace = "com.clp3z.projectb.framework.network"
+}
+
+/*
 android {
     namespace = "com.clp3z.projectb.framework.network"
     compileSdk = 35
@@ -23,15 +31,40 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
 }
+*/
 
 dependencies {
 
+    // Modules
+    implementation(project(":entity"))
+
+    // Hilt
+    implementation(libs.google.dagger.hilt.android)
+    kapt(libs.google.dagger.hilt.compiler)
+
+    // Coroutines
+    implementation(libs.bundles.kotlinx.coroutines)
+
+    // Serialization
+    implementation(libs.kotlinx.serialization.json)
+
+    // Arrow
+    implementation(libs.arrow.core)
+
+    // Retrofit
+    implementation(libs.bundles.squareup.retrofit)
+}
+
+kapt {
+    correctErrorTypes = true
 }
